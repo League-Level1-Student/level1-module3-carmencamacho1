197be093d957661e2aa9cffb6957335be7004b1b
Student_2 <Student_2@165.24.36.142>
2018-04-30 13:42:53 -0700

    netflix, movie, movie2

--- src/Movie2.java
+++ src/Movie2.java
@@ -7,5 +7,16 @@ public static void main(String[] args) {
 	Movie spray = new Movie("Hairspray", 4);
 	Movie tales = new Movie("Veggie Tales", 1);
 	String a = fault.getTicketPrice();
-}
+
+	NetflixQueue net = new NetflixQueue();
+	net.addMovie(fault );
+	net.addMovie(panther);
+	net.addMovie(dory);
+	net.addMovie(spray);
+	net.addMovie(tales);
+net.printMovies();
+Movie b = net.getBestMovie();
+System.out.println("The best movie is " + b);
+
+	}
 }
